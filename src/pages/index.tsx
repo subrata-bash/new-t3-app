bac906229a7313a5976bdf67626fc7e922e0a82f
Subrata Ghosh Dastidar <github.subrata@gmail.com>
2023-03-26 00:56:28 +0530

    font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,7 @@ const PostView = (props: PostWithUser) => {
           <span>{`@${author.username}`}</span> •{" "}
           <span>{dayjs(post.createdAt).fromNow()}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
